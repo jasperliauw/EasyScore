3fa5170261ccf07bb09872a2a0f39acaca5bd598
jasperliauw <jasperliauw@gmail.com>
2023-10-24 21:47:01 +0800

    Added reminder

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="CSSFiles", template_folder="HTMLFiles")
 
+print("Remember to flush chrome sockets!")
+
 
 @app.route("/")
 def Index():
