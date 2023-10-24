3e5bb2fcfcd22a416bdd11b75a6509438474403e
Jasper Liauw <jasperliauw@gmail.com>
2023-10-24 20:27:12 +0800

    Changed main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# https://www.youtube.com/watch?v=dam0GPOAvVI&list=LL&index=3&t=7560s
-print("Hello World!")
+from flask import Flask, render_template
 
-from website import create_app
+app = Flask(__name__, static_folder="CSSFiles", template_folder="HTMLFiles")
 
-app = create_app()
 
-if __name__ == '__main__':
+@app.route("/")
+def Index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
